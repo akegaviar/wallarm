fd0eef4cb0c303acbfdf3cb3980e2cc7a77e8f9e
Evgeny Konstantinov <ekonstantinov@wallarm.com>
2017-05-11 12:14:59 +0300

    Set proper copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Документация Wallarm'
-copyright = u'2017, Wallarm'
+copyright = u'2017, Wallarm Inc'
 author = u'Evgeny Konstantinov'
 
 # The version info for the project you're documenting, acts as replacement for
